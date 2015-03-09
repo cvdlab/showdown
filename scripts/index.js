6ed38cb6b02451276db807fcd2b08314ff92ef86
enricomarino <enrico.marino@email.com>
2015-03-09 11:35:33 +0100

    vulcanized

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,5 @@
 (function () {
-  var slides = document.querySelector('x-slides');
 
-  var qs = queryString.parse(location.search);
-  var md = qs.md;
-
-  if (!md) {
-    md = 'README.md';
-  }
-
-  md = md.replace('github.com/', 'rawgit.com/').replace('/blob/', '/');
-
-  superagent.get(md).end(function (res) {
-    if (!res.ok) {
-      console.error(res);
-      return;
-    }
-    slides.markdown = res.text;
-  });
-
-  var plugins = slides.querySelectorAll('[plugin]').array();
-  plugins.forEach(function (plugin) {
-    plugin.slides = slides;
-  });
+  console.log('SHOWDOWN!');
 
 }());
